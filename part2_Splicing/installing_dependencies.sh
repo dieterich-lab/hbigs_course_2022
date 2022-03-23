cecea06cbbd869b0036d48ef25653594e381b154
Thiago Britto-Borges <thiago.brittoborges@uni-heidelberg.de>
2022-03-23 08:12:23 -0300

    update

--- part2_Splicing/installing_dependencies.sh
+++ part2_Splicing/installing_dependencies.sh
@@ -6,7 +6,7 @@ pip install snakemake --user
 pip install jupyter --user
 
 # R packages
-Rscript -e 'install.packages(c("BiocManager", "knitr", "devtools", "rstan",  "rstantools", "R.utils", "Hmisc", "foreach", "dplyr", "doMC", "optparse", "shinyjs", "intervals", "shinycssloaders", "IRkernel"), lib="~/R/x86_64-pc-linux-gnu-library/3.6", repos="https://cran.uni-muenster.de", INSTALL_opts = c("--no-lock"))'
+Rscript -e 'install.packages(c("BiocManager", "knitr", "devtools",  "R.utils", "Hmisc", "dplyr", "stringr", "IRkernel"), lib="~/R/x86_64-pc-linux-gnu-library/3.6", repos="https://cran.uni-muenster.de", INSTALL_opts = c("--no-lock"))'
 
 # IRkernel
 Rscript -e 'IRkernel::installspec()'
